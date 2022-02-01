55333b891810ebfcd408443c5caae41967a41df1
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2022-02-01 18:22:03 +0000

    Basic app VII

---
+++ app/src/main/kotlin/com/redkey/keyboard/input/RedKeyInputMethodService.kt
@@ -0,0 +1,22 @@
+package com.redkey.keyboard.input
+
+import android.inputmethodservice.InputMethodService
+import android.view.View
+import com.redkey.keyboard.databinding.KeyboardViewBinding
+
+class RedKeyInputMethodService : InputMethodService, View.OnClickListener {
+    override fun onCreateInputView(): View {
+        val binding = KeyboardViewBinding.inflate(layoutInflater)
+        setupListeners(binding)
+
+        return binding.root
+    }
+
+    override fun onClick(view: View) {
+        //TODO: Handle button presses
+    }
+
+    private fun setupListeners(binding: KeyboardViewBinding) {
+        //TODO: Setup listeners
+    }
+}
