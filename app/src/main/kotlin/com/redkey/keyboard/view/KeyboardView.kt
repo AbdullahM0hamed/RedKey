bdf1e7491ce91724226873a117ccece5d8b53ffc
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2022-02-03 20:16:58 +0000

    Test changing colour

--- app/src/main/kotlin/com/redkey/keyboard/view/KeyboardView.kt
+++ app/src/main/kotlin/com/redkey/keyboard/view/KeyboardView.kt
@@ -1,6 +1,7 @@
 package com.redkey.keyboard.view
 
 import android.content.Context
+import android.graphics.Canvas
 import android.view.View
 
 class KeyboardView(
@@ -12,5 +13,10 @@ class KeyboardView(
         heightMeasureSpec: Int
     ) {
         setMeasuredDimension(100, 100)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawColor(0xFFFF0000.toInt())
     }
 }
