cfb82545f13f2dbd4c547478cbaa4c848ccf89a5
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2022-02-03 01:12:23 +0000

    Basic app XI

--- app/src/main/kotlin/com/redkey/keyboard/input/RedKeyInputMethodService.kt
+++ app/src/main/kotlin/com/redkey/keyboard/input/RedKeyInputMethodService.kt
@@ -2,13 +2,15 @@ package com.redkey.keyboard.input
 
 import android.inputmethodservice.InputMethodService
 import android.view.View
+import android.view.ViewGroup.LayoutParams
 import com.redkey.keyboard.R
 import com.redkey.keyboard.databinding.KeyboardViewBinding
 
 class RedKeyInputMethodService : InputMethodService(), View.OnClickListener {
     override fun onCreateInputView(): View {
         val binding = KeyboardViewBinding.inflate(layoutInflater)
-        android.widget.Toast.makeText(this, binding.root.width.toString(), 5).show()
+        setViewHeightDp(binding.root, 100)
+        android.widget.Toast.makeText(this, binding.root.height.toString(), 5).show()
 
         return binding.constraint
     }
@@ -19,5 +21,12 @@ class RedKeyInputMethodService : InputMethodService(), View.OnClickListener {
 
     private fun setupListeners(binding: KeyboardViewBinding) {
         //TODO: Setup listeners
+    }
+    
+    private fun setViewHeightDp(view: View, height: Float) {
+        val d = service.resources.displayMetrics.density
+        val px = height * d + 0.5f
+
+        view.setLayoutParams(LayoutParams(-1, px.toInt()))
     }
 }
