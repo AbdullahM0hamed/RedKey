f665a2e7e34d13a8de7221167a4e3d57e773b70f
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2022-05-05 22:49:16 +0100

    Add keyboard row V

--- app/src/main/kotlin/com/redkey/keyboard/input/RedKeyInputMethodService.kt
+++ app/src/main/kotlin/com/redkey/keyboard/input/RedKeyInputMethodService.kt
@@ -1,7 +1,6 @@
 package com.redkey.keyboard.input
 
 import android.inputmethodservice.InputMethodService
-import android.inputmethodservice.Keyboard
 import android.view.View
 import android.view.ViewGroup.LayoutParams
 import com.redkey.keyboard.R
@@ -10,9 +9,10 @@ import com.redkey.keyboard.view.KeyboardView
 class RedKeyInputMethodService : InputMethodService(), View.OnClickListener {
     override fun onCreateInputView(): View {
         val keyboard = KeyboardView(this)
+        keyboard.setBackgroundColor(0xFFFF0000.toInt())
         val view = View(this)
         view.layoutParams = LayoutParams(keyboard.width, keyboard.height / 5)
-        view.setBackgroundColor(0xFF000000.toInt())
+        view.setBackgroundColor(0xFFFFFFFF.toInt())
 
         keyboard.addView(view)
         return KeyboardView(this)
